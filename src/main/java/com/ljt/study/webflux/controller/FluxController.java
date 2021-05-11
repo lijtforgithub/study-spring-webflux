c28b3ee15e653a680f0c41291f3139e99ddd3b8d
李敬堂 <lijingtang@sanweiguoye.onaliyun.com>
2021-05-11 13:51:42 +0800

    flux

---
+++ src/main/java/com/ljt/study/webflux/controller/FluxController.java
@@ -0,0 +1,27 @@
+package com.ljt.study.webflux.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
+
+/**
+ * @author LiJingTang
+ * @date 2021-05-10 17:53
+ */
+@Slf4j
+@RestController
+@RequestMapping("/flux")
+public class FluxController {
+
+    @GetMapping(produces = TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> index() {
+        return Flux.interval(Duration.ofSeconds(1)).take(10);
+    }
+
+}
